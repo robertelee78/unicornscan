68681cb9fcfc6674bd17356b08e5ed7b3d703271
Robert E. Lee <robert@loveathome.us>
2026-01-25 17:20:33 -0800

    Fix runtime version and bug report URL in packageinfo.h
    - Update VERSION from stale 0.4.22 to current 0.4.50
    - Update BUGURL from SourceForge mailing list to GitHub issues

--- src/packageinfo.h
+++ src/packageinfo.h
@@ -1,7 +1,7 @@
 #ifndef VERSION
 
-#define VERSION "0.4.22"
-#define BUGURL "osace-users@lists.sourceforge.net"
+#define VERSION "0.4.50"
+#define BUGURL "https://github.com/robertelee78/unicornscan/issues"
 
 #ifdef WITH_SELINUX
 #define _B_SE " SELinux"
